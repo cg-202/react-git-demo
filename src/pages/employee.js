d0312f2ab7ce0efc4932bd03401e23d0996d11f1
santosh <santosh.ece06@gmail.com>
2021-07-09 10:33:45 +0530

    asdfasf

--- src/pages/employee.js
+++ src/pages/employee.js
@@ -1,3 +1,19 @@
 export function Employee() {
-  return <h1>Hello WOrld</h1>;
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus nam
+        reiciendis ullam? Officiis expedita non quibusdam quos excepturi vel
+        obcaecati minus facere facilis amet. Corrupti illo eos adipisci sapiente
+        ipsa!
+      </h1>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate
+        accusamus aliquid cupiditate odit blanditiis nihil magni error modi
+        beatae, corporis ducimus repellendus aspernatur veniam cum. Fuga
+        delectus nulla eius perferendis!
+      </p>
+    </div>
+  );
 }
